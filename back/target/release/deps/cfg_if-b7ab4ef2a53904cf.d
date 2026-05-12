37ed08579f0e72a32947ad6c8270ea9b53d49446
paulscc <paul.sc9@hotmail.com>
2026-05-12 08:38:10 -0500

    7

---
+++ back/target/release/deps/cfg_if-b7ab4ef2a53904cf.d
@@ -0,0 +1,7 @@
+C:\Proyectos\ProyectChat\back\target\release\deps\cfg_if-b7ab4ef2a53904cf.d: C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Proyectos\ProyectChat\back\target\release\deps\libcfg_if-b7ab4ef2a53904cf.rlib: C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Proyectos\ProyectChat\back\target\release\deps\libcfg_if-b7ab4ef2a53904cf.rmeta: C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs:
---
+++ back/target/release/deps/log-5e74debe7fb47c7f.d
@@ -0,0 +1,10 @@
+C:\Proyectos\ProyectChat\back\target\release\deps\log-5e74debe7fb47c7f.d: C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\lib.rs C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\macros.rs C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\serde.rs C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\__private_api.rs
+
+C:\Proyectos\ProyectChat\back\target\release\deps\liblog-5e74debe7fb47c7f.rlib: C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\lib.rs C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\macros.rs C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\serde.rs C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\__private_api.rs
+
+C:\Proyectos\ProyectChat\back\target\release\deps\liblog-5e74debe7fb47c7f.rmeta: C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\lib.rs C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\macros.rs C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\serde.rs C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\__private_api.rs
+
+C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\lib.rs:
+C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\macros.rs:
+C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\serde.rs:
+C:\Users\pcstu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\log-0.4.29\src\__private_api.rs:
